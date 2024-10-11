2fef1877616e663fbc8def1ec28ad68f9ff5ed69
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-10-11 19:48:31 +0200

    Main character movement

---
+++ Assets/Scripts/MainMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+    }
+}
